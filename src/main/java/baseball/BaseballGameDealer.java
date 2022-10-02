dfda82d716ed172da630bc74c5d5dcdbc250ba49
coldMater <coldmater@gmail.com>
2022-10-03 00:06:55 +0900

    feat: 게임 진행 관련 Controller 로직 추가

--- src/main/java/baseball/BaseballGameDealer.java
+++ src/main/java/baseball/BaseballGameDealer.java
@@ -1,12 +1,43 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class BaseballGameDealer {
+    BaseballGame baseballGame;
     public void start() {
-        readDigits();
+        do {
+            this.runOneGame();
+        } while (askRestartGame());
+    }
+
+    private void runOneGame() {
+        setNewGame();
+        while (true) {
+            ThreeDigits td = readDigits();
+            BaseballGameResult result = this.baseballGame.compare(td);
+            BaseballGameOutput.printOneResult(result);
+            if (result.isWin()) {
+                BaseballGameOutput.printYouWin();
+                break;
+            }
+        }
+    }
+
+    private void setNewGame() {
+        List<Integer> numList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        this.baseballGame = new BaseballGame(numList.get(0), numList.get(1), numList.get(2));
+        System.out.printf("디버깅모드: 대상 숫자는 %d%d%d%s", numList.get(0), numList.get(1), numList.get(2), System.lineSeparator());
     }
 
     private ThreeDigits readDigits() {
         BaseballGameOutput.printPleaseInputDigits();
         return BaseballGameInput.readThreeDigits();
+    }
+
+    private boolean askRestartGame() {
+        BaseballGameOutput.printWhetherRestartGame();
+        return BaseballGameInput.readRestartGame();
     }
 }
