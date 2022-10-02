467d3b61658bc820c49f2dff89a7de527d060e97
coldMater <coldmater@gmail.com>
2022-10-03 00:09:50 +0900

    chore: 디버그용 콘솔 아웃풋 제거

--- src/main/java/baseball/BaseballGameDealer.java
+++ src/main/java/baseball/BaseballGameDealer.java
@@ -28,7 +28,7 @@ public class BaseballGameDealer {
     private void setNewGame() {
         List<Integer> numList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         this.baseballGame = new BaseballGame(numList.get(0), numList.get(1), numList.get(2));
-        System.out.printf("디버깅모드: 대상 숫자는 %d%d%d%s", numList.get(0), numList.get(1), numList.get(2), System.lineSeparator());
+        // System.out.printf("Debug Only Target Number:  %d%d%d%s", numList.get(0), numList.get(1), numList.get(2), System.lineSeparator());
     }
 
     private ThreeDigits readDigits() {
